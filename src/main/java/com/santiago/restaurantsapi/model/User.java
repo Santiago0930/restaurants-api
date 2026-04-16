e802e0ee9e4c8961243ec710153545e26cdd567b
Santiago0930 <santiago.guerrerod@javeriana.edu.co>
2026-04-16 14:41:50 -0500

    Configure MySQL connection

--- src/main/java/com/santiago/restaurantsapi/model/User.java
+++ src/main/java/com/santiago/restaurantsapi/model/User.java
@@ -3,8 +3,6 @@ package com.santiago.restaurantsapi.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
@@ -12,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
