072c257df258d18f25f97948067188d1938472c8
Santiago0930 <santiago.guerrerod@javeriana.edu.co>
2026-04-16 13:11:04 -0500

    Initial commit

---
+++ src/main/java/com/santiago/restaurantsapi/RestaurantsApiApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.restaurantsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsApiApplication.class, args);
+	}
+
+}
